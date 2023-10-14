e18afcc9d2ea16256a9688a511898f45b7d7875e
handras <7267096+handras@users.noreply.github.com>
2023-10-14 14:41:29 +0200

    playing with coroutines

--- Manager.gd
+++ Manager.gd
@@ -13,6 +13,8 @@ var tile_collector_script = preload("res://Scripts/TileCollector.gd")
 
 var tile_collector = tile_collector_script.new()
 
+signal fill_next_factory
+
 func fillFactory(factory):
 	print('filling factory')
 	var _tiles = tile_collector.get_tiles(factory.MaxTiles)
@@ -20,16 +22,17 @@ func fillFactory(factory):
 
 
 func _ready():
+	fill_factories()
 	for col in TileColors:
 		var _mat = StandardMaterial3D.new()
 		_mat.albedo_color = col
 		_mat.roughness = 0.351
 		matList.append(_mat)
-	for i in range(3):
+	for i in range(5):
 		var _fac = tiles_factory.instantiate()
 		factories.append(_fac)
 		add_child(_fac)
-		var alpha = 2*PI*i/3.0 + randf_range(deg_to_rad(-2), deg_to_rad(3));
+		var alpha = 2*PI*i/5.0 + randf_range(deg_to_rad(-2), deg_to_rad(3));
 		_fac.position = Vector3(1*sin(alpha), 0, -1*cos(alpha))
 		var rot = [0, 90, 180, 270][randi()%4]+randfn(0, 3.2)
 		_fac.rotation = Vector3(0, rad_to_deg(rot), 0)
@@ -49,10 +52,16 @@ func _physics_process(_delta):
 	var result = space_state.intersect_ray(query)
 	print(result)
 
+
+func fill_factories():
+	await fill_next_factory
+	for fac in self.factories:
+		fillFactory(fac)
+		await fill_next_factory
+
+
 @warning_ignore("unused_parameter")
 func _process(delta):
 	if Input.is_action_just_pressed("LeftClick"):
-		fillFactory(factories[0])
-		fillFactory(factories[1])
-		fillFactory(factories[2])
+		fill_next_factory.emit()
 
